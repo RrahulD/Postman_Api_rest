2c331ebe9369befbf1a6c34e4a4494185048e3a7
RrahulD <rr999987@gmail.com>
2021-09-13 17:13:23 +0530

    Django Application

---
+++ mywebApp/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from mywebApp.models import Users
+
+class UsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields =( 'id',
+        'name' ,
+        'age',
+        'sex',
+         'bloodg',
+         'disease',
+        )
